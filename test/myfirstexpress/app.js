b06af628f8b0b1d504f8076fed3ce2b74e13446d
syl <1712673874@qq.com>
2017-02-23 13:55:13 +0800

    20170223syl

--- test/myfirstexpress/app.js
+++ test/myfirstexpress/app.js
@@ -1,18 +1,18 @@
-var express = require('express');
+var express = require('express'); //加载express模块
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/index');//访问默认首页的地址
 var users = require('./routes/users');
 
-var app = express();
+var app = express(); //express模块执行，赋值给app变量
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views')); //设置视图的物理路径
+app.set('view engine', 'jade'); //设置视图引擎为jade
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', routes); //访问默认首页的地址
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -56,5 +56,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
+module.exports.name="syl";
 module.exports = app;
