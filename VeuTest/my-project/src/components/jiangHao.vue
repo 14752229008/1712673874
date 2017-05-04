f58aa4d66824e25f741b4220832779cf6fae5cc1
syl <1712673874@qq.com>
2017-05-04 18:40:44 +0800

    vue_dome

---
+++ VeuTest/my-project/src/components/jiangHao.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="jiangHao">
+        <div>{{youName}}</div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'jiangHao',
+  data () {
+    return {
+      youName: '蒋日天与叶良辰'
+    }
+  }
+}
+</script>
